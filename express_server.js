9341afbc85f7d80109e1dc6dc3eae0467a63ccfc
Radia Ahmed <rad@Radias-Air.ht.home>
2023-05-01 22:54:23 -0400

    changed the port

--- express_server.js
+++ express_server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 7777; // default port 8080
 
-app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
+
+app.get("/hello", (req, res) => {
+    res.send("<html><body>Hello <b>World</b></body></html>\n");
   });
 
 const urlDatabase = {
@@ -16,6 +17,6 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Example app listening on port:`, PORT);
 });
 

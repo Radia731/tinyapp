4f00d34038a9b01c3d80a08151fd7cecf9504c93
Radia Ahmed <rad@Radias-Air.ht.home>
2023-05-02 00:11:20 -0400

    adding Route and Template

--- express_server.js
+++ express_server.js
@@ -1,25 +1,31 @@
-
 const express = require("express");
 const app = express();
 const PORT = 7777; // default port 8080
 
 app.set("view engine", "ejs") //tells the Express app to use EJS as its templating engine
 
-
-app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
-  });
-
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {  // setting the routes 
+
+app.get("/hello", (req, res) => {
+    res.send("<html><body>Hello <b>World</b></body></html>\n");
+    });
+app.get("/urls.json", (req, res) => { // setting routes
+    res.json(urlDatabase);
+  });
+  
+app.get("/urls/:id", (req, res) => {
+    const templateVars = { id: req.params.id, longURL};
+    res.render("urls_show", templateVars);
+  });
+
+app.get("/", (req, res) => {  
   res.send("Hello!");
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port:`, PORT);
 });
-

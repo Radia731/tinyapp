ba5bed96b12ecb248675ea84bd3b5a572d193c5a
Radia Ahmed <rad@Radias-Air.ht.home>
2023-05-04 07:04:59 -0400

    GET route to expressserver.js and render using accompanying template

--- express_server.js
+++ express_server.js
@@ -3,12 +3,21 @@ const app = express();
 const PORT = 7777; // default port 8080
 
 app.set("view engine", "ejs") //tells the Express app to use EJS as its templating engine
-
+app.use(express.urlencoded({ extended: true })); // adding a middle ware 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+    const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let result = '';
+    for (let i = 0; i < 6; i++) {
+      result += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return result;
+  }
+  
 
 app.get("/hello", (req, res) => {
     res.send("<html><body>Hello <b>World</b></body></html>\n");
@@ -17,6 +26,10 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => { // setting routes
     res.send(urlDatabase);
   });
+
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });  
   
 app.get("/urls/:id", (req, res) => {
     const templateVars = { id: req.params.id, longURL:urlDatabase[req.params.id]};
@@ -27,6 +40,13 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.post("/urls", (req, res) => {
+    console.log(req.body); // Log the POST request body to the console
+    res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  });
+
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port:`, PORT);
 });
+

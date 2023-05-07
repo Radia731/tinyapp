22d7a43173f75150e1f38e3e41deaf5c513e6121
Radia Ahmed <rad@Radias-Air.ht.home>
2023-05-07 11:55:09 -0400

    browser back to /urls page after successful login

--- express_server.js
+++ express_server.js
@@ -70,7 +70,13 @@ app.post("/urls", (req, res) => {
     urlDatabase[shortURL] = longURL;
     res.redirect("/urls");
   });
-
+  
+  app.post('/login', (req, res) => {
+    const { username } = req.body;
+    res.cookie('username', username);
+    res.redirect('/urls');
+  });
+  
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port:`, PORT);

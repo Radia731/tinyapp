2844317d3e93b3447a8bcb285288a71061f32d5e
Radia Ahmed <rad@Radias-Air.ht.home>
2023-05-08 19:04:49 -0400

    register updated

--- express_server.js
+++ express_server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require('bcrypt');
 const app = express();
 const PORT = 8999; // default port 7777
 
@@ -11,6 +12,19 @@ app.use(express.urlencoded({ extended: true }));
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
+};
+
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
 };
 
 function generateRandomString() {
@@ -27,15 +41,20 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
+  const userId = req.cookies.user_id;
+  const user = users[userId];
   const templateVars = {
     urls: urlDatabase,
-    username: req.cookies.username
+    username: req.cookies.username,
+    user: user
   };
-  res.render("urls_index", templateVars);
+  res.render('urls_index', templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
+  const userId = req.cookies.user_id;
   const templateVars = { 
+    user: users[userId],
     username: req.cookies.username,
     id: req.params.id, 
     longURL: urlDatabase[req.params.id] 
@@ -44,19 +63,35 @@ app.get("/urls/:id", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars = {
-    username: req.cookies["username"]
-  };
-  res.render("urls_new", templateVars);
+  const userId = req.cookies.user_id;
+  const user = users[userId];
+  if (!user) {
+    res.redirect('/login');
+  } else {
+    const templateVars = {
+      user
+    };
+    res.render('urls_new', templateVars);
+  }
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = {
-    shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL],
-    username: req.cookies["username"]
+  const userId = req.cookies.user_id;
+  const user = users[userId];
+  const shortURL = req.params.shortURL;
+  const url = urlDatabase[shortURL];
+  if (!url) {
+    return res.status(404).send('URL does not exist');
+  }
+  if (url.userId !== userId) {
+    return res.status(403).send('You do not have access to this URL');
+  }
+  const templateVars = { 
+    shortURL,
+    longURL: url.longURL,
+    user
   };
-  res.render("urls_show", templateVars);
+  res.render('urls_show', templateVars);
 });
 
 app.get("/", (req, res) => {
@@ -79,25 +114,73 @@ app.post("/urls/:shortURL", (req, res) => {
   res.redirect("/urls");
 });
 
-app.get('/register', (req, res) => {
-  res.render('register');
-});
-
 app.get("/register", (req, res) => {
   res.render("register");
 });
 
 
+app.post("/register", (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).send("Email and password fields are required.");
+    return;
+  }
+
+  // check if email already exists
+  const existingUser = findUserByEmail(email, users);
+  if (existingUser) {
+    res.status(400).send("Account already exists.");
+    return;
+  }
+
+  const id = generateRandomString();
+  const newUser = {
+    id,
+    email,
+    password: bcrypt.hashSync(password, 10),
+  };
+  users[id] = newUser;
+
+  res.cookie("user_id", id);
+  res.redirect("/urls");
+});
+
+function findUserByEmail(email, users) {
+  for (const userId in users) {
+    const user = users[userId];
+    if (user.email === email) {
+      return user;
+    }
+  }
+  return null;
+}
+
+function generateRandomString() {
+  return Math.random().toString(36).substr(2, 6);
+}
+
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+
 app.post("/login", (req, res) => {
-  const { username } = req.body;
-  res.cookie("username", username);
+  const { email, password } = req.body;
+  const user = findUserByEmail(email, users);
+  if (!user || !bcrypt.compareSync(password, user.password)) {
+    res.status(403).send("Invalid email or password");
+    return;
+  }
+  res.cookie("user_id", user.id);
   res.redirect("/urls");
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("username");
-  res.redirect("/urls");
+  res.clearCookie("user_id");
+  res.redirect("/login");
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);

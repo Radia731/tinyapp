661da58a601c4eb99130f9348be9f56117f4acdb
Radia Ahmed <rad@Radias-Air.ht.home>
2023-05-19 16:17:22 -0400

    decalration adjustment in server side

--- express_server.js
+++ express_server.js
@@ -26,12 +26,12 @@ app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   b6UTxQ: {
-    longURL: "https://www.tsn.ca",
-    userID: "aJ48lW",
+    longURL: "https://www.TD.ca",
+    userID: "john000",
   },
   i3BoGr: {
-    longURL: "https://www.google.ca",
-    userID: "aJ48lW",
+    longURL: "https://www.instagram.ca",
+    userID: "bella001",
   },
 };
 
@@ -71,7 +71,7 @@ function generateRandomString() {
 }
 
 function requireLogin(req, res, next) {
-  if (!req.session.user_id) {
+  if (!req.session.userID) {
     res.redirect('/login');
   } else {
     next();
@@ -92,14 +92,27 @@ const checkAuth = (email, pass) => {
 };
 
 
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 
+// GET Redirect localhost to /urls page.
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.redirect("/urls/");
 });
+
+// GET Display JSON Data
+app.get("/data.json", (req, res) => {
+  const data = {
+    urlDatabase,
+    users
+  };
+  res.set("Content-Type", "application/json");
+  res.send(JSON.stringify(data, null, 2));
+});
+////////////////////Only list url for the logged in user///////////////////////////
 
 function urlsForUser(id) {
   const userURLs = {};
@@ -113,47 +126,8 @@ function urlsForUser(id) {
 
 //////////////////ROUTES/////////////////////
 app.get("/urls", (req, res) => {
-  const userId = req.cookies.user_id;
-  const user = users[userId];
-  if (!user) {
-    res.redirect('/login');
-  } else {
-    const userURLs = urlsForUser(userId);
-    const templateVars = {
-      urls: userURLs,
-      username: req.cookies.username,
-      user: user
-    };
-    res.render('urls_index', templateVars);
-  }
-});
-
-/////////////////////ID ROUTE//////////////////
-app.get("/urls/:id", (req, res) => {
-  const userId = req.cookies.user_id;
-  const user = users[userId];
-  const urlId = req.params.id;
-  if (!user) {
-    res.redirect("/login");
-  } else if (!urlDatabase[urlId]) {
-    res.status(404).send("URL not found");
-  } else if (urlDatabase[urlId].userID !== userId) {
-    res.status(403).send("You do not have access to this URL");
-  } else {
-    const templateVars = { 
-      user: users[userId],
-      username: req.cookies.username,
-      id: urlDatabase, 
-      longURL: urlDatabase[urlId].longURL 
-    };
-    res.render("urls_show", templateVars);
-  }
-});
-
-
-//////////////////////////////NEW route ///////////////////////////////////
-app.get("/urls/new", (req, res) => {
-  const userId = req.cookies.user_id;
+  const userId = req.session.userID;
+  console.log("userId", userId)
   const user = users[userId];
   console.log("user", user)
   if (!user) {
@@ -191,7 +165,7 @@ app.get("/urls/:shortURL", (req, res) => {
 app.post("/urls", requireLogin, (req, res) => {
   const longURL = req.body.longURL;
   const shortURL = generateRandomString();
-  const userId = req.session.user_id;
+  const userId = req.session.userID;
   urlDatabase[shortURL] = { longURL, userId };
   res.redirect(`/urls/${shortURL}`);
 });
@@ -206,6 +180,8 @@ app.post("/urls/:shortURL", (req, res) => {
   urlDatabase[req.params.shortURL] = req.body.longURL;
   res.redirect("/urls");
 });
+
+
 /////////////////////////////  REGISTER//////////////////////////////
 app.get("/register", (req, res) => {
   const userId = req.session.userID;
@@ -222,51 +198,8 @@ app.get("/register", (req, res) => {
 
 /////////////////////////////////////LOGIN//////////////////////////////////////////////
 
-
-app.post("/register", (req, res) => {
-  const { email, password } = req.body;
-  if (!email || !password) {
-    res.status(400).send("Email and password fields are required.");
-    return;
-  }
-
-  ///////////////////check if email already exists////////////////////////////////////
-  const existingUser = findUserByEmail(email, users);
-  if (existingUser) {
-    res.status(400).send("Account already exists.");
-    return;
-  }
-
-  const id = generateRandomString();
-  const newUser = {
-    id,
-    email,
-    password: bcrypt.hashSync(password, 10),
-  };
-  users[id] = newUser;
-
-  res.cookie("user_id", id);
-  res.redirect("/urls");
-});
-
-function findUserByEmail(email, users) {
-  for (const userId in users) {
-    const user = users[userId];
-    if (user.email === email) {
-      return user;
-    }
-  }
-  return null;
-}
-
-function generateRandomString() {
-  return Math.random().toString(36).substr(2, 6);
-}
-
-/////////////////////////////////////LOGIN//////////////////////////////////////////////
-
 app.get("/login", (req, res) => {
-  const userId = req.cookies.user_id;
+  const userId = req.session.userID;
   const user = users[userId]
   if (user) {
     res.redirect("/urls");

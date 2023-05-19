c22a52fa850738fffae8945afd1a0e8f7f51c43a
Radia Ahmed <rad@Radias-Air.ht.home>
2023-05-19 16:52:05 -0400

    decalration adjustment in server side

--- express_server.js
+++ express_server.js
@@ -7,7 +7,7 @@ const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));git 
 
 const urlDatabase = {
   b6UTxQ: {
